0d1d25bfcbdd4478ecae69abb313b91be1c778ba
szitastamas <szitastamas@hotmail.com>
2020-03-05 10:58:42 +0100

    Got stuck at Sudoku 3x3 Block validation

--- app.js
+++ app.js
@@ -322,6 +322,7 @@ const checkPinCodeValidity = (pin) => {
     return true;
 }
 
+// Longest combination of two strings with unique values
 const str1 = "asdgasa";
 const str2 = "asdfasgh";
 const longest = (s1, s2) => {
@@ -330,4 +331,125 @@ const longest = (s1, s2) => {
     return [...new Set(combination)].join("");
 }
 
-console.log(longest(str1, str2))
+//console.log(longest(str1, str2))
+
+// Sorting only ODD numbers in a number array with the EVEN numbers staying in place
+function sortArray(array) {
+  
+    if(array.length === 0) return array;
+  
+    const indexOfEvenNumbers = array.reduce((acc, num, index) => num % 2 === 0 ? [...acc, {[num]: index}] : acc ,[]);
+    
+    const oddNumbersArray = array.filter(num => num % 2 !==0).sort((a,b) => a-b);
+
+    indexOfEvenNumbers.forEach(numObject => {
+        const key = Object.keys(numObject)
+        const value = Object.values(numObject);
+        
+        oddNumbersArray.splice(value, 0, +key);
+
+    });
+    
+    return oddNumbersArray;
+  }
+
+//console.log(sortArray([1,5,2,3,9,4]));
+
+// Create a phone number from an array of 10 integers
+function createPhoneNumber(numbers){
+    const firstThree = numbers.splice(0,3).join("");
+    const secondThree = numbers.splice(0,3).join("");
+
+    return `(${firstThree}) ${secondThree}-${numbers.join("")}`;
+  }
+
+  console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]))
+
+
+  // Finding the digital root of a number
+  function digital_root(n) {
+    return n > 9 ? digital_root([...n.toString()].reduce((acc, num) => acc+=(+num),0)) : n;
+  }
+
+  //console.log(digital_root(132189))
+
+  // Counting how many characters occur more than once in a string
+  function duplicateCount(text){
+    const toBeCheckedText = [...text.toLowerCase()];
+
+    const counterObject = toBeCheckedText.map(letter => {
+        return{
+            ...letter,
+            counter: toBeCheckedText.filter(l => l === letter).length
+        }
+    })
+    .filter(obj => obj.counter !== 1);
+
+    return new Set(counterObject.map(count => count[0])).size;
+  }
+
+  //console.log(duplicateCount("AbBaac"))
+
+  // Return the position (NOT THE INDEX) of the only odd / even number in the array
+  function iqTest(numbers){
+
+    const dictionary = {
+        odd: [],
+        even: []
+    };
+    numbers.split(" ").forEach((elem, index) => +elem % 2 !== 0 ? dictionary.odd.push({elem, index}) : dictionary.even.push({elem, index}))
+    return dictionary.odd.length === 1 ? dictionary.odd[0].index+1 : dictionary.even[0].index+1;
+  }
+
+//   console.log(iqTest("1 3 2 5"))
+//   console.log(iqTest("2 4 8 1"))
+
+// Move all the 0s to the end of the array
+const moveZeros = function (arr) {
+    const arrayOfZeros = arr.reduce((acc,elem) => elem === 0 ? [...acc, elem] : acc ,[]);
+    const arrayWithoutZeros = arr.filter(elem => elem !== 0);
+
+    return [...arrayWithoutZeros, ...arrayOfZeros];
+  }
+
+  //console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
+
+  // Validate SUDOKU Solutions
+
+const validSolution = (board) => {
+
+    let isValid = true;
+
+    for(let [index, row] of board.entries()) {
+        if(row.includes(0)) {
+            isValid = false;
+            break;
+        };
+
+        if(new Set(row).size !== row.length){
+            isValid = false;
+            break;
+        }
+
+        const column = board.map(row => row[index]);
+        if(new Set(column).size !== column.length){
+            isValid = false;
+            break;
+        }
+    }
+
+    // Implement 3x3 block validation --> each block must contain all 9 numbers
+    return isValid;
+}
+
+  console.log(validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ]));
